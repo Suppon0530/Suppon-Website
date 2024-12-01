9f144419cb7e13a18f422ede5a7ff8bfc1a47faa
Suppon0530 <Suppon9089@gmail.com>
2024-12-01 11:25:38 +0000

    Footerコンポーネントの修正

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,48 +1,32 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
 
 // フッターコンポーネント
 export default function Footer() {
+  // ページトップへのスクロール関数
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <footer className='grid-rows-3 bg-main py-6'>
-      <div className='text-center'>
-        <div className='row-start-1 flex justify-center space-x-4'>
-          <Link href='/contact' className='block text-base hover:text-accent'>
+    <footer className='grid grid-rows-2 place-content-center place-items-center bg-main py-8'>
+      <div className='row-start-1'>
+        <div className='flex'>
+          <Link href='/contact' className='px-4 text-base hover:text-accent'>
             お問い合わせ
           </Link>
-          <Link href='/privacy' className='block text-base hover:text-accent'>
+          <Link href='/privacy' className='px-4 text-base hover:text-accent'>
             プライバシーポリシー
           </Link>
         </div>
-        <div className='row-start-2'>
-          <a
-            href='https://twitter.com/Suppon0530'
-            className='text-base hover:text-white'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            X
-          </a>
-          <span className='text-base'>　|　</span>
-          <a
-            href='https://github.com/Suppon0530'
-            className='text-base hover:text-white'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            GitHub
-          </a>
-          <span className='text-base'>　|　</span>
-          <a
-            href='https://www.youtube.com/@Suppon0530_GameDeveloper'
-            className='text-base hover:text-white'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            YouTube
-          </a>
-        </div>
-        <div className='row-start-3'>
+      </div>
+      <div className='row-start-2'>
+        <div className='flex items-center justify-center'>
           <p className='text-base'>&copy; {new Date().getFullYear()} Suppon Game.</p>
         </div>
       </div>
