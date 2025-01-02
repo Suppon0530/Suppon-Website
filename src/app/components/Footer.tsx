9fbd3cd19d306d84cb4a3e3d2df20de0eb5431c6
Suppon0530 <Suppon9089@gmail.com>
2025-01-02 12:01:54 +0000

    Footerコンポーネントの修正

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -14,20 +14,27 @@ export default function Footer() {
   };
 
   return (
-    <footer className='grid grid-rows-2 place-content-center place-items-center bg-main py-8'>
-      <div className='row-start-1'>
-        <div className='flex'>
-          <Link href='/contact' className='px-4 text-base hover:text-accent'>
-            お問い合わせ
-          </Link>
-          <Link href='/privacy' className='px-4 text-base hover:text-accent'>
-            プライバシーポリシー
-          </Link>
+    <footer>
+      <div className='bg-white-color flex flex-row items-center justify-evenly'>
+        <div className='flex flex-shrink-0 flex-col items-center justify-center py-4'>
+          <div className='flex flex-row items-center justify-center text-sm'>
+            <Link href='/contact' className='px-4 text-base-color hover:text-main-color'>
+              お問い合わせ
+            </Link>
+            <Link href='/privacy' className='px-4 text-base-color hover:text-main-color'>
+              プライバシーポリシー
+            </Link>
+          </div>
+          <div className='flex-auto'>
+            <div className='flex items-center justify-center text-sm'>
+              <p className='pt-1 text-base-color'>&copy; {new Date().getFullYear()} Suppon Game.</p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className='row-start-2'>
-        <div className='flex items-center justify-center'>
-          <p className='text-base'>&copy; {new Date().getFullYear()} Suppon Game.</p>
+        <div className='flex items-center justify-end font-bold'>
+          <button className='rounded-full bg-main-color px-4 py-2' onClick={scrollToTop}>
+            ↑
+          </button>
         </div>
       </div>
     </footer>
